51f83aca9a10aaf4ee375a56c54831c69ed33aef
02ggang9 <02ggang9@gmail.com>
2023-11-09 19:18:17 +0900

    feat(WeekdayDiscountEvent): 할인 금액을 반환하는 기능 구현

--- src/main/java/christmas/domain/discount/WeekdayDiscountEvent.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscountEvent.java
@@ -1,4 +1,14 @@
 package christmas.domain.discount;
 
+import java.util.ArrayList;
+
 public class WeekdayDiscountEvent {
+
+    private static final Integer BASE_DISCOUNT_AMOUNT = 2023;
+
+    public int discountAmount(int dessertCount) {
+        return BASE_DISCOUNT_AMOUNT * dessertCount;
+    }
+
+
 }
