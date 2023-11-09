32f6904efd8a53286f1e3e21e5b7859a9c68039b
02ggang9 <02ggang9@gmail.com>
2023-11-09 17:24:23 +0900

    feat(christmasController): 사용자가 주문할 메뉴를 입력받도록 Controller 계층에게 책임 위임

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -3,14 +3,34 @@ package christmas.controller;
 import christmas.service.ChristmasService;
 import christmas.view.ChristmasInputView;
 
+import java.util.List;
+
 public class ChristmasController {
 
     private final ChristmasInputView christmasInputView = new ChristmasInputView();
     private final ChristmasService christmasService = new ChristmasService();
 
     public void run() {
+        // STEP1 : 예상 방문 날짜 입력받고 저장
         int dateOfVisit = getDateOfVisit();
         saveDateOfVisit(dateOfVisit);
+
+        // STEP2 : 주문할 메뉴와 개수 입력 받고 저장
+        List<String> orderMenuAndCount = getOrderMenuAndCount();
+        saveOrderMenu(orderMenuAndCount);
+        saveOrderMenuCount(orderMenuAndCount);
+    }
+
+    private void saveOrderMenuCount(List<String> orderMenuAndCount) {
+
+    }
+
+    private void saveOrderMenu(List<String> orderMenuAndCount) {
+
+    }
+
+    private List<String> getOrderMenuAndCount() {
+        return christmasInputView.getUserOrderMenu();
     }
 
     private void saveDateOfVisit(int dateOfVisit) {
