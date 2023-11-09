581a1c80a86c299c009efdfdf4172804c8d526c7
02ggang9 <02ggang9@gmail.com>
2023-11-09 19:21:53 +0900

    feat(SpecialDiscountEvent): 할인 금액을 반환하는 기능 구현

--- src/main/java/christmas/domain/discount/SpecialDiscountEvent.java
+++ src/main/java/christmas/domain/discount/SpecialDiscountEvent.java
@@ -1,4 +1,20 @@
 package christmas.domain.discount;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpecialDiscountEvent {
+
+    List<Integer> days = List.of(3, 10, 17, 24, 25, 31);
+
+    private static final int BASE_DISCOUNT_AMOUNT = 1000;
+
+    public int discountAmount(int dateOfVisit, int mainMenuCount) {
+        if (days.contains(dateOfVisit)) {
+            return BASE_DISCOUNT_AMOUNT * mainMenuCount;
+        }
+
+        return 0;
+    }
+
 }
