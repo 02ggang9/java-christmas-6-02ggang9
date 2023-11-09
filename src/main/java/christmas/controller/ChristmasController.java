0d94456abd3219047d5f3d36391b499b12717bf4
02ggang9 <02ggang9@gmail.com>
2023-11-09 17:10:31 +0900

    feat(ChristmasController): 사용자가 입력한 예상 방문 날짜를 저장하도록 Service 계층의 메서드에 위임

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,14 +1,20 @@
 package christmas.controller;
 
+import christmas.service.ChristmasService;
 import christmas.view.ChristmasInputView;
 
 public class ChristmasController {
 
     private final ChristmasInputView christmasInputView = new ChristmasInputView();
+    private final ChristmasService christmasService = new ChristmasService();
 
     public void run() {
         int dateOfVisit = getDateOfVisit();
+        saveDateOfVisit(dateOfVisit);
+    }
 
+    private void saveDateOfVisit(int dateOfVisit) {
+        christmasService.saveDateOfVisit(dateOfVisit);
     }
 
     private int getDateOfVisit() {
