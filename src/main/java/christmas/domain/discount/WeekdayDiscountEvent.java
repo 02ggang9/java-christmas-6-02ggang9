f7a7acf6e48d412be5033641ba44762feb6b2f41
02ggang9 <02ggang9@gmail.com>
2023-11-09 19:24:28 +0900

    fix(WeekdayDiscountEvent): 할인 금액이 적용되는 날짜를 추가

--- src/main/java/christmas/domain/discount/WeekdayDiscountEvent.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscountEvent.java
@@ -1,13 +1,20 @@
 package christmas.domain.discount;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WeekdayDiscountEvent {
 
+    List<Integer> days = List.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31);
+
     private static final Integer BASE_DISCOUNT_AMOUNT = 2023;
 
-    public int discountAmount(int dessertCount) {
-        return BASE_DISCOUNT_AMOUNT * dessertCount;
+    public int discountAmount(int dateOfVisit, int dessertCount) {
+        if (days.contains(dateOfVisit)) {
+            return BASE_DISCOUNT_AMOUNT * dessertCount;
+        }
+
+        return 0;
     }
 
 
