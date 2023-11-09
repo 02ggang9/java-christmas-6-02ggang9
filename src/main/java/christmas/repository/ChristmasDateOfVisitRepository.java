1fcc5ed0621baa961427d5eeb403360aeccbbb0c
02ggang9 <02ggang9@gmail.com>
2023-11-09 17:13:56 +0900

    feat(ChristmasService): 사용자가 입력한 예상 방문 날짜를 저장하도록 Repository 계층의 메서드에 위임

---
+++ src/main/java/christmas/repository/ChristmasDateOfVisitRepository.java
@@ -0,0 +1,14 @@
+package christmas.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChristmasDateOfVisitRepository {
+
+    private final Map<Long, Integer> dateOfVisitDatabase = new HashMap<>();
+
+    public void saveDateOfVisit(int dateOfVisit) {
+
+    }
+
+}
--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -1,9 +1,13 @@
 package christmas.service;
 
+import christmas.repository.ChristmasDateOfVisitRepository;
+
 public class ChristmasService {
 
-    public void saveDateOfVisit(int dateOfVisit) {
+    private final ChristmasDateOfVisitRepository repository = new ChristmasDateOfVisitRepository();
 
+    public void saveDateOfVisit(int dateOfVisit) {
+        repository.saveDateOfVisit(dateOfVisit);
     }
 
 }
