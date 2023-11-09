627ece08886682373d28f113a2d3396524d6ca23
02ggang9 <02ggang9@gmail.com>
2023-11-09 19:14:37 +0900

    feat(ChristmasDiscountEvent): 할인 금액을 반환하는 기능 구현

---
+++ src/main/java/christmas/domain/discount/ChristmasDiscountEvent.java
@@ -0,0 +1,13 @@
+package christmas.domain.discount;
+
+public class ChristmasDiscountEvent {
+
+    private static final Integer BASE_DISCOUNT_AMOUNT = 1000;
+
+    public ChristmasDiscountEvent() {
+    }
+
+    public int discountAmount(int dateOfVisit) {
+        return BASE_DISCOUNT_AMOUNT + ((dateOfVisit-1) * 100);
+    }
+}
