7596408ce725fa55b376a7808196c4488dec4ce1
02ggang9 <02ggang9@gmail.com>
2023-11-10 20:14:02 +0900

    fix(OrderMenu): Enum을 사용해 MenuType을 표현하도록 수정

--- src/main/java/christmas/domain/menu/OrderMenu.java
+++ src/main/java/christmas/domain/menu/OrderMenu.java
@@ -3,35 +3,33 @@ package christmas.domain.menu;
 import java.util.Arrays;
 import java.util.function.Function;
 
+import static christmas.domain.menu.MenuType.*;
+
 public enum OrderMenu {
 
-    // 애피타이저
-    MUSHROOM_SOUP("양송이수프", "에피타이저", 6_000L, (menuType) -> menuType.equals("에피타이저")),
-    TAPAS("타파스", "에피타이저", 5_500L, (menuType) -> menuType.equals("에피타이저")),
-    CAESAR_SALAD("시저샐러드", "에피타이저", 8_000L, (menuType) -> menuType.equals("에피타이저")),
+    MUSHROOM_SOUP("양송이수프", APPETIZER, 6_000L, (menuType) -> menuType.equals("에피타이저")),
+    TAPAS("타파스", APPETIZER, 5_500L, (menuType) -> menuType.equals("에피타이저")),
+    CAESAR_SALAD("시저샐러드", APPETIZER, 8_000L, (menuType) -> menuType.equals("에피타이저")),
 
-    // 메인
-    TBONE_STEAK("티본스테이크", "메인", 55_000L, (menuType) -> menuType.equals("메인")),
-    BARBECUE_RIBS("바비큐립", "메인", 54_000L, (menuType) -> menuType.equals("메인")),
-    SEAFOOD_PASTA("해산물파스타", "메인", 35_000L, (menuType) -> menuType.equals("메인")),
-    CHRISTMAS_PASTA("크리스마스파스타", "메인", 25_000L, (menuType) -> menuType.equals("메인")),
+    TBONE_STEAK("티본스테이크", MAIN, 55_000L, (menuType) -> menuType.equals("메인")),
+    BARBECUE_RIBS("바비큐립", MAIN, 54_000L, (menuType) -> menuType.equals("메인")),
+    SEAFOOD_PASTA("해산물파스타", MAIN, 35_000L, (menuType) -> menuType.equals("메인")),
+    CHRISTMAS_PASTA("크리스마스파스타", MAIN, 25_000L, (menuType) -> menuType.equals("메인")),
 
-    // 디저트
-    CHOCOLATE_CAKE("초코케이크", "디저트", 15_000L, (menuType) -> menuType.equals("디저트")),
-    ICE_CREAM("아이스크림", "디저트", 5_000L, (menuType) -> menuType.equals("디저트")),
+    CHOCOLATE_CAKE("초코케이크", DESSERT, 15_000L, (menuType) -> menuType.equals("디저트")),
+    ICE_CREAM("아이스크림", DESSERT, 5_000L, (menuType) -> menuType.equals("디저트")),
 
-    // 음료
-    ZERO_COKE("제로콜라", "음료", 3_000L, (menuType) -> menuType.equals("음료")),
-    RED_WINE("레드와인", "음료", 60_000L, (menuType) -> menuType.equals("음료")),
-    CHAMPAGNE("샴페인", "음료", 25_000L, (menuType) -> menuType.equals("음료")),
+    ZERO_COKE("제로콜라", DRINK, 3_000L, (menuType) -> menuType.equals("음료")),
+    RED_WINE("레드와인", DRINK, 60_000L, (menuType) -> menuType.equals("음료")),
+    CHAMPAGNE("샴페인", DRINK, 25_000L, (menuType) -> menuType.equals("음료")),
     ;
 
     private final String MenuName;
-    private final String MenuType;
+    private final MenuType MenuType;
     private final Long MenuPrice;
     private final Function<String, Boolean> function;
 
-    OrderMenu(String menuName, String menuType, Long menuPrice, Function<String, Boolean> function) {
+    OrderMenu(String menuName, MenuType menuType, Long menuPrice, Function<String, Boolean> function) {
         MenuName = menuName;
         MenuType = menuType;
         MenuPrice = menuPrice;
@@ -42,7 +40,7 @@ public enum OrderMenu {
         return MenuName;
     }
 
-    public String getMenuType() {
+    public MenuType getMenuType() {
         return MenuType;
     }
 
