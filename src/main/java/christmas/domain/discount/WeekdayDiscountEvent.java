0676d43345c8735b7f7aa94fb8787619c46ea35b
02ggang9 <02ggang9@gmail.com>
2023-11-11 18:15:39 +0900

    feat(WeekdayDiscountEvent): 상속을 이용해 평일 할인을 계산하도록 구현

--- src/main/java/christmas/domain/discount/WeekdayDiscountEvent.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscountEvent.java
@@ -1,21 +1,38 @@
 package christmas.domain.discount;
 
+import christmas.domain.OrderSheet;
+import christmas.domain.detail.EventDetail;
+import christmas.domain.discountpolicy.DiscountPolicy;
+import christmas.domain.menu.MenuType;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class WeekdayDiscountEvent {
+public class WeekdayDiscountEvent extends Discount {
 
-    List<Integer> days = List.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31);
-
+    private static final String DISCOUNT_NAME = "평일 할인: -";
     private static final Integer BASE_DISCOUNT_AMOUNT = 2023;
 
-    public int discountAmount(int dateOfVisit, int dessertCount) {
-        if (days.contains(dateOfVisit)) {
-            return BASE_DISCOUNT_AMOUNT * dessertCount;
-        }
-
-        return 0;
+    public WeekdayDiscountEvent(DiscountPolicy... policies) {
+        super(policies);
     }
 
+    @Override
+    public void calculateDiscountAndSaveDetail(EventDetail eventDetail, OrderSheet orderSheet) {
+        for (DiscountPolicy policy : policies) {
+            if (policy.isSatisfiedBy(orderSheet)) {
+                eventDetail.saveEvent(DISCOUNT_NAME, discountPrice(orderSheet));
+            }
+        }
+    }
 
+    private int discountPrice(OrderSheet orderSheet) {
+        return BASE_DISCOUNT_AMOUNT * orderSheet.getOrders()
+                .entrySet()
+                .stream()
+                .filter(o -> o.getKey().getMenuType() == MenuType.DESSERT)
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+    }
 }
