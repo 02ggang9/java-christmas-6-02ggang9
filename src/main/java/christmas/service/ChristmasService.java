21e0455d42ebbc79fd8098c0499820b8d6bd337f
02ggang9 <02ggang9@gmail.com>
2023-11-11 19:13:45 +0900

    chore(ChristmasService): 상속을 이용한 후 미사용 메서드 삭제

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -22,57 +22,14 @@ public class ChristmasService {
         repository.saveDateOfVisit(dateOfVisit);
     }
 
-//    public Integer calculateTotalAmount(Set<Map.Entry<String, Integer>> orderMenus) {
-//        int totalAmount = 0;
-//        for (Map.Entry<String, Integer> orderMenu : orderMenus) {
-//            OrderMenu menu = OrderMenu.getOrderMenu(orderMenu.getKey());
-//            totalAmount += (orderMenu.getValue() * menu.getMenuPrice());
-//        }
-//
-//        return totalAmount;
-//    }
-
     public Integer calculateTotalAmount() {
         OrderSheet findOrderSheet = christmasOrderMenuRepository.findOrderSheetById(0L);
         return findOrderSheet.calculateTotalPrice();
     }
 
-//    public GiveMenu getGiveMenu(Integer amount) {
-//        if (GiveMenu.CHAMPAGNE.isOver(amount)) {
-//            return GiveMenu.CHAMPAGNE;
-//        }
-//
-//        return GiveMenu.NOTING;
-//    }
-
     public GiveMenu getGiveMenu(Integer price) {
         return GiveMenu.getGiveMenu(price);
     }
-
-//    public int getChristmasDiscountAmount() {
-//        int dateOfVisit = repository.findDateOfVisit();
-//        return ChristmasDiscountEvent.discountAmount(dateOfVisit);
-//    }
-
-//    public int getWeekdayDiscountAmount() {
-//        WeekdayDiscountEvent weekdayDiscountEvent = new WeekdayDiscountEvent();
-//        int dateOfVisit = repository.findDateOfVisit();
-//        int dessertCount = christmasOrderMenuRepository.findDessertCount();
-//        return weekdayDiscountEvent.discountAmount(dateOfVisit, dessertCount);
-//    }
-
-//    public int getWeekendDiscountAmount() {
-//        WeekendDiscountEvent weekendDiscountEvent = new WeekendDiscountEvent();
-//        int dateOfVisit = repository.findDateOfVisit();
-//        int mainCount = christmasOrderMenuRepository.findMainCount();
-//        return weekendDiscountEvent.discountAmount(dateOfVisit, mainCount);
-//    }
-
-//    public int getSpecialDiscountAmount() {
-//        SpecialDiscountEvent specialDiscountEvent = new SpecialDiscountEvent();
-//        int dateOfVisit = repository.findDateOfVisit();
-//        return specialDiscountEvent.discountAmount(dateOfVisit);
-//    }
 
     public OrderSheet findOrderSheet() {
         return christmasOrderMenuRepository.findOrderSheetById(0L);
