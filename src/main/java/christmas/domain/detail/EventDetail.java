cc674024b14afeb83d53a6e8f01a3760bbd4d69b
02ggang9 <02ggang9@gmail.com>
2023-11-11 18:37:13 +0900

    feat(EventDetail): 상속을 혜택 내역을 저장하도록 구현

--- src/main/java/christmas/domain/detail/EventDetail.java
+++ src/main/java/christmas/domain/detail/EventDetail.java
@@ -1,8 +1,8 @@
 package christmas.domain.detail;
 
 import christmas.domain.OrderSheet;
-import christmas.domain.discount.ChristmasDiscountEvent;
-import christmas.domain.discount.Discount;
+import christmas.domain.discount.*;
+import christmas.domain.discountpolicy.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +11,13 @@ import java.util.Map;
 public class EventDetail {
 
     private static final List<Discount> events = List.of(
-            new ChristmasDiscountEvent()
+            new ChristmasDiscountEvent(new ChristmasDiscountPolicy()),
+            new GiveMenuDiscountEvent(new GiveMenuDiscountPolicy()),
+            new SpecialDiscountEvent(new SpecialDiscountPolicy()),
+            new WeekdayDiscountEvent(new WeekdayDiscountPolicy()),
+            new WeekendDiscountEvent(new WeekendDiscountPolicy())
     );
+
     private final Map<String, Integer> details;
 
     public EventDetail(Map<String, Integer> details) {
@@ -26,6 +31,6 @@ public class EventDetail {
     }
 
     public void saveEvent(String eventName, int discountPrice) {
-
+        details.put(eventName, discountPrice);
     }
 }
