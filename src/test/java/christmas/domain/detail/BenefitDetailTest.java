a4843b63242dc31767f7876996c16be15bf0ad19
02ggang9 <02ggang9@gmail.com>
2023-11-13 23:44:47 +0900

    test(BenefitDetailTest): 복수 이벤트 정보 저장 테스트

--- src/test/java/christmas/domain/detail/BenefitDetailTest.java
+++ src/test/java/christmas/domain/detail/BenefitDetailTest.java
@@ -1,9 +1,14 @@
 package christmas.domain.detail;
 
+import christmas.domain.OrderSheet;
+import christmas.domain.menu.OrderMenu;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static christmas.domain.menu.OrderMenu.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,7 +17,13 @@ class BenefitDetailTest {
     @Test
     @DisplayName("성공적으로 이벤트 정보들을 저장해야 한다")
     void 성공적으로_이벤트_정보들을_저장해야_한다() {
+        int expectedDiscountPrice = 8269;
+        BenefitDetail benefitDetail = new BenefitDetail();
+        OrderSheet orderSheet = new OrderSheet(List.of(CHOCOLATE_CAKE.getMenuName(), TBONE_STEAK.getMenuName()), List.of(3,4), 3);
 
+        benefitDetail.saveEventDetails(orderSheet);
+
+        assertThat(benefitDetail.getTotalBenefitPrice()).isEqualTo(expectedDiscountPrice);
     }
 
     @Test
