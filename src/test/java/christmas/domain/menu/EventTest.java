b6698b1aaa37d4250ce1fde9cf219e5978cd6ce2
02ggang9 <02ggang9@gmail.com>
2023-11-13 11:42:57 +0900

    test(EventTest): 이벤트 메뉴 반환 추가 테스트

--- src/test/java/christmas/domain/menu/EventTest.java
+++ src/test/java/christmas/domain/menu/EventTest.java
@@ -17,4 +17,11 @@ class EventTest {
         assertThat(findEventMenu).isEqualTo(Event.CHAMPAGNE);
     }
 
+    @Test
+    @DisplayName("이벤트 메뉴가 없는 경우 NOTING을 반환해야 한다.")
+    void 이벤트_메뉴가_없는_경우_NOTING을_반환해야_한다() {
+        assertThat(Event.getGiveMenu(10_000)).isEqualTo(Event.NOTING);
+    }
+
+
 }
