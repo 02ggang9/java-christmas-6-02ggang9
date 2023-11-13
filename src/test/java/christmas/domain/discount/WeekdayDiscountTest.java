1d2b55638add51d259bbead8623056ba2a42a4ac
02ggang9 <02ggang9@gmail.com>
2023-11-13 18:11:45 +0900

    test(WeekdayDiscountTest): 평일 할인 실패 테스트

--- src/test/java/christmas/domain/discount/WeekdayDiscountTest.java
+++ src/test/java/christmas/domain/discount/WeekdayDiscountTest.java
@@ -33,4 +33,19 @@ class WeekdayDiscountTest {
         assertThat(benefitDetail.getDetails().get("평일 할인: -")).isEqualTo(discountPrice * (chocoCount + iceCreamCount));
     }
 
+    @Test
+    @DisplayName("평일 할인에 실패해야 한다.")
+    void 평일_할인에_실패해야_한다() {
+        int day = 9;
+        int mainCount = 3;
+        int drinkCount = 4;
+        BenefitDetail benefitDetail = new BenefitDetail();
+        OrderSheet orderSheet = new OrderSheet(List.of(TBONE_STEAK.getMenuName(), ZERO_COKE.getMenuName()), List.of(mainCount, drinkCount), day);
+        WeekdayDiscount weekdayDiscount = new WeekdayDiscount(new WeekdayDiscountPolicy());
+
+        weekdayDiscount.calculateDiscountAndSaveDetail(benefitDetail, orderSheet);
+
+        assertThat(benefitDetail.getDetails().containsKey("평일 할인: -")).isFalse();
+    }
+
 }
