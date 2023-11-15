24130d913e7c7d501e2bb79d8bb76971f99ac3b2
02ggang9 <02ggang9@gmail.com>
2023-11-15 11:20:58 +0900

    test(ChristmasInputViewTest): 예약 날짜 입력 테스트

---
+++ src/test/java/christmas/view/ChristmasInputViewTest.java
@@ -0,0 +1,32 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ChristmasInputViewTest {
+
+    @ParameterizedTest
+    @DisplayName("예약 날짜 입력 테스트")
+    @ValueSource(ints = {1,2,3,4,5,6,7,31})
+    void 예약_날짜_입력_테스트(int dateOfVisit) {
+        MockedStatic<Console> consoleMockedStatic = mockStatic(Console.class);
+        when(Console.readLine()).thenReturn(String.valueOf(dateOfVisit));
+
+        ChristmasInputView christmasInputView = new ChristmasInputView();
+        christmasInputView.getUserDateOfVisit();
+        assertThatCode(christmasInputView::getUserDateOfVisit).doesNotThrowAnyException();
+
+        consoleMockedStatic.close();
+    }
+
+}
